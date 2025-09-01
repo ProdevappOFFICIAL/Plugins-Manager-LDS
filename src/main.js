e291825fbd8e0de1c48f9369cc36b5b73542e928
ProdevappOFFICIAL <ProdevappOFFICIAL@users.noreply.github.com>
2025-09-01 09:22:28 +0000

    fix: Correct calculation error in tax computation

--- src/main.js
+++ src/main.js
@@ -5,3 +5,4 @@ console.log('Hello, World!');
 // Added feature: Mon Aug 25 15:55:17 UTC 2025
 // Added feature: Thu Aug 28 09:19:36 UTC 2025
 // Fixed bug on Thu Aug 28 14:10:09 UTC 2025
+// Fixed bug on Mon Sep  1 09:22:28 UTC 2025
